71c53012449325debe6155cdcf1c65c79366ca87
sghaedi84-wq <sghaedi84@gmail.com>
2025-11-28 22:47:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiohttp import web
+from aiogram.utils.executor import start_polling
 
 TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
-@dp.message()
+@dp.message_handler()
 async def handle_message(message: types.Message):
-    await message.answer(f"پیام دریافت شد:\n{message.text}")
-
-async def on_startup(app):
-    await bot.set_webhook(WEBHOOK_URL)
-
-async def on_shutdown(app):
-    await bot.delete_webhook()
-
-def main():
-    app = web.Application()
-    dp.router.bind(app)
-    SimpleRequestHandler(dp, bot).register(app, path="/webhook")
-    setup_application(app, dp, bot=bot)
-
-    app.on_startup.append(on_startup)
-    app.on_shutdown.append(on_shutdown)
-
-    web.run_app(app, host="0.0.0.0", port=10000)
+    await message.reply(f"پیام دریافت شد:\n{message.text}")
 
 if __name__ == "__main__":
-    main()
+    start_polling(dp, skip_updates=True)
